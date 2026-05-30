4fdfc1a2caa8c48ffd6d1eb5960c3eba83a9ff46
Nexa Pixel <johanessuaktiwa@gmail.com>
2026-05-30 18:51:04 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   darkMode: 'class',
   theme: {
+    fontSize: {
+      xs: ['0.8125rem', { lineHeight: '1rem' }],
+      sm: ['0.9375rem', { lineHeight: '1.25rem' }],
+      base: ['1.0625rem', { lineHeight: '1.5rem' }],
+      lg: ['1.1875rem', { lineHeight: '1.75rem' }],
+      xl: ['1.3125rem', { lineHeight: '1.75rem' }],
+      '2xl': ['1.5625rem', { lineHeight: '2rem' }],
+      '3xl': ['1.9375rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2.3125rem', { lineHeight: '2.5rem' }],
+      '5xl': ['3.0625rem', { lineHeight: '1' }],
+      '6xl': ['3.8125rem', { lineHeight: '1' }],
+      '7xl': ['4.5625rem', { lineHeight: '1' }],
+      '8xl': ['6.0625rem', { lineHeight: '1' }],
+      '9xl': ['8.0625rem', { lineHeight: '1' }]
+    },
     extend: {
       colors: {
         coach: {
