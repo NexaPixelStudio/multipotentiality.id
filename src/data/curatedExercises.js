145c191972509b170056107a4fa6c4c37bf0b319
Nexa Pixel <johanessuaktiwa@gmail.com>
2026-06-02 22:07:33 +0700

    Connect conditional calculation exercises

--- src/data/curatedExercises.js
+++ src/data/curatedExercises.js
@@ -1,11 +1,14 @@
 import { batchBasicSalesTable, batch01BasicCalculationExercises } from './quality/batch01BasicCalculation.js';
+import { batchConditionalOrderTable, batch02ConditionalCalculationExercises } from './quality/batch02ConditionalCalculation.js';
 
 export const sharedExerciseTables = {
-  batchBasicSales: batchBasicSalesTable
+  batchBasicSales: batchBasicSalesTable,
+  batchConditionalOrders: batchConditionalOrderTable
 };
 
 export const curatedExercises = {
-  ...batch01BasicCalculationExercises
+  ...batch01BasicCalculationExercises,
+  ...batch02ConditionalCalculationExercises
 };
 
 export function getCuratedExercise(formulaId) {
